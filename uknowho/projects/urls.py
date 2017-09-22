2bcd9364181ce62438e2c3a7af94ac89337100e7
caldasdeoliveira <diogocaldasdeoliveira@gmail.com>
2017-09-23 00:07:30 +0100

    first stage newprojectform and detail for project, just sentences

--- uknowho/projects/urls.py
+++ uknowho/projects/urls.py
@@ -6,5 +6,9 @@ app_name = 'projects'
 urlpatterns = [
 	# /projects/
     url(r'^$', views.index, name='index'),
-    
+    #/projects/12345/
+    url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
+    #/projects/newProjectForm/
+    url(r'^projects/newProjectForm$',views.newProjectForm, name='newProjectForm'),
+
 ]
--- uknowho/projects/views.py
+++ uknowho/projects/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'projects/base.html', {})
+
+def detail(request,project_id):
+	return HttpResponse("<h2> Details for project"+str(project_id)+"</h2>")
+
+def newProjectForm(request):
+	return HttpResponse("<h1> New Project form</h1>")

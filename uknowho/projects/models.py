f5851df2b500a37698c350f8202f235a698c47e3
Duarte Botelho <duartebotelho@ist.utl.pt>
2017-09-23 06:13:57 +0100

    fuck this

--- uknowho/projects/models.py
+++ uknowho/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from user.models import Profile
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -22,3 +23,32 @@ class Project(models.Model):
     colaborators = Colaborators()
     labels = Labels()
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, default = None)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    phoneNumber = models.IntegerField()
+    #email = models.CharField(max_length=50)
+    linkedIn = models.CharField(max_length=50)
+    facebook = models.CharField(max_length=50)
+    twitter = models.CharField(max_length=50)
+    photo = models.CharField(max_length=20)
+
+    def get_absolute_url(self):
+        return reverse('user:detail', kwargs={'pk': self.pk})
+
+
+class Skill(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    skill = models.CharField(max_length=50)
+
+
+class Language(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    language = models.CharField(max_length=40)

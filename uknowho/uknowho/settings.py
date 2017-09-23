01ecb5c0bb2d83f94e97a5f8237f471ea157d05d
Mrrvm <mrrvm@hotmail.com>
2017-09-23 06:37:29 +0100

    Deleted app user

--- uknowho/uknowho/settings.py
+++ uknowho/uknowho/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user.apps.UserConfig',
     'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- uknowho/uknowho/urls.py
+++ uknowho/uknowho/urls.py
@@ -19,6 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('projects.urls')),
-    url(r'^user/', include('user.urls')),
     url(r'^projects/', include('projects.urls')),    
 ]

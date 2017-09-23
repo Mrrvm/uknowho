5703dca14533b7331b3a2ec7c2af20c9687a8851
Mrrvm <mrrvm@hotmail.com>
2017-09-23 07:57:42 +0100

    i guess its solved

--- uknowho/projects/models.py
+++ uknowho/projects/models.py
@@ -1,36 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 # Create your models here.
-
-
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    userid = models.CharField(default=' ', max_length=50)
+    password = models.CharField(default=' ', max_length=50)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
     university = models.CharField(max_length=50)
-    phoneNumber = models.IntegerField()
     #email = models.CharField(max_length=50)
     linkedIn = models.CharField(max_length=50)
     facebook = models.CharField(max_length=50)
     twitter = models.CharField(max_length=50)
     photo = models.CharField(max_length=20)
 
-    def get_absolute_url(self):
-        return reverse('user:detail', kwargs={'pk': self.pk})
+    #def get_absolute_url(self):
+    #    return reverse('user:detail', kwargs={'pk': self.pk})
 
 class Labels(models.Model):
     label = models.CharField(max_length=30)#review
 
-class Colaborators(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default = None)
-
-#class location(models.Model):
-
+# Create a profile first.
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
@@ -39,18 +32,21 @@ class Project(models.Model):
     size = models.CharField(max_length=100)
     projectType = models.IntegerField()
     #postdate = models.DateField()
-    colaborators = Colaborators()
     labels = Labels()
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, default = None)
 
 
-
-
 class Skill(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    userid = models.ForeignKey(Profile, on_delete=models.CASCADE)
     skill = models.CharField(max_length=50)
 
 
 class Language(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    userid = models.ForeignKey(Profile, on_delete=models.CASCADE)
     language = models.CharField(max_length=40)
+
+
+class Colaborators(models.Model):
+    userid = models.ForeignKey(Profile, on_delete=models.CASCADE, default = None)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, default = None)
+

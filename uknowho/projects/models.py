0b076446e4cb19d5d7ba62a938e140b60934eed8
Duarte Botelho <duartebotelho@ist.utl.pt>
2017-09-23 06:19:46 +0100

    fuck this, but it works :)

--- uknowho/projects/models.py
+++ uknowho/projects/models.py
@@ -4,6 +4,25 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    phoneNumber = models.IntegerField()
+    #email = models.CharField(max_length=50)
+    linkedIn = models.CharField(max_length=50)
+    facebook = models.CharField(max_length=50)
+    twitter = models.CharField(max_length=50)
+    photo = models.CharField(max_length=20)
+
+    def get_absolute_url(self):
+        return reverse('user:detail', kwargs={'pk': self.pk})
+
 class Labels(models.Model):
     label = models.CharField(max_length=30)#review
 
@@ -25,23 +44,6 @@ class Project(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, default = None)
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    course = models.CharField(max_length=50)
-    university = models.CharField(max_length=50)
-    phoneNumber = models.IntegerField()
-    #email = models.CharField(max_length=50)
-    linkedIn = models.CharField(max_length=50)
-    facebook = models.CharField(max_length=50)
-    twitter = models.CharField(max_length=50)
-    photo = models.CharField(max_length=20)
-
-    def get_absolute_url(self):
-        return reverse('user:detail', kwargs={'pk': self.pk})
 
 
 class Skill(models.Model):
